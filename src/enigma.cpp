c154f0ef2a87f9ce2ab49f911d299c506e33fd13
Rob de Witte <inifire201@gmail.com>
2019-01-16 22:11:53 +0100

    push for tomorrow

--- src/enigma.cpp
+++ src/enigma.cpp
@@ -1,4 +1,5 @@
 //here goes all the non phisical techical stuff
+template <size_t rows, size_t cols>
 
 class Enigma{
     //TODO add the rest of these rotors
@@ -9,30 +10,89 @@ class Enigma{
     int rotorIII [26] = {};
     int rotorIV  [26] = {};
 
+    int reflectorB [13][2] = {
+        {1,25},
+        {2,18},
+        {3,21},
+        {4,8 },
+        {5,17},
+        {6,19},
+        {7,12},
+        {9,16},
+        {10,24},
+        {11,14},
+        {13,15},
+        {20,26},
+        {22,23}
+    };
 
-    int rotorset1;
-    int rotorset2;
-    int rotorset3;
-    int reflectorset;
+    int getReflector(int reflector[][], int posA, int posB){
+        return reflector[posA - 1][posB - 1];
+    }
 
-    int translate(int rotor[], int symbolIn, int positionIn){
-        //TODO checken van spagetti code
-        int pos = symbolIn + positionIn;
-        if(pos < 26){
-            pos =- 26;
-        }
+    int getRotor(int rotor[], int pos){
+        return rotor[pos - 1];
+    }
 
-        // \/ that just doesn't work
-        for(int i=0; rotorRef[i] == symbolIn; i++){
-            pos = i;
-        }
-        
-        if(rotorRef[pos] > rotor[pos]){
-            return symbolIn - pos;
+    int translateIn(int rotor[], int symbolIn, int rotorPos){
+            if(symbolIn > rotorPos){
+                return getRotor(rotor, symbolIn - rotorPos);
+            }
+            else{
+                return getRotor(rotor, 26 + symbolIn - rotorPos);
+            }
+    }
+
+
+    //TODO probably still broken
+    int translateOut(int rotor[], int symbolIn, int rotorPos){
+        int insertPos;
+        //May have broken something
+        //Checks if the position is retractable from the rotor
+        if(getRotor(rotor, symbolIn) > rotorPos){
+            insertPos = getRotor(rotor, symbolIn) - rotorPos;
         }
         else{
-            return symbolIn + 26 - pos;
+            insertPos = 26 - (getRotor(rotor, symbolIn) - rotorPos);
         }
+    }
+
+    int reflect(int reflector[][],int symbolIn){
+        int x[];
+        for(int i = 0; i < 13; i++){
+            
+            for(int j = 0; j < 2; j++){
+                
+                //looks for the symbol in the reflector
+                if(getReflector(reflector,i,j) == symbolIn){
+                    x = {i,j};
+                    break;
+                }
+            }
+
+            //Tests if the symbol is found yet
+            if(x != NULL){
+                break;
+            }
+        }
+
+        if(x[1] == 0){
+            return getReflector(reflector,x[0],1);
+        }
+        else{
+            return getReflector(reflector,x[0],0)
+        }
+    }
+    
+    int encrypt(int rotor1[], int rotor2[], int rotor3[], int reflector[][], int pos1, int pos2, int pos3, int symbolIn){
+        int symbol;
+        symbol = translateIn(rotor1, symbolIn, pos1);
+        symbol = translateIn(rotor2, symbol, pos2);
+        symbol = translateIn(rotor3, symbol, pos3);
+        symbol = reflect(reflector[][], symbol);
+        symbol = translateOut(rotor3, symbol, pos3);
+        symbol = translateOut(rotor2, symbol, pos2);
+        return translateOut(rotor1, symbol, pos1);
     }
 
 }
--- src/functional.cpp
+++ src/functional.cpp
@@ -1,3 +1,10 @@
 //here goes al the code that makes the physical stuff work
-
+#include<LedControl.h>
 #include<enigma.cpp>
+
+
+class display{
+    void setDisplay(){
+        
+    }
+}
